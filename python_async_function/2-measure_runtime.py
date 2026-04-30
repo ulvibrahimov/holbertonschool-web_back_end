74c920b2f4db5330f8832752d8cac5f082be7e85
ulvibrahimov <ulvibrahimov@gmail.com>
2026-04-30 16:41:06 +0000

    Fix total_time variable for Task 2 checker and update Task 4 loop

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,25 +2,26 @@
 """
 Module to measure runtime of concurrent coroutines.
 """
-import time
 import asyncio
+import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     """
-    Measures the total execution time for wait_n and returns time per task.
+    Measures the total execution time for wait_n(n, max_delay).
 
     Args:
         n (int): The number of coroutines launched.
         max_delay (int): The max delay parameter.
 
     Returns:
-        float: Average execution time per coroutine.
+        float: total_time / n
     """
-    start_time = time.time()
+    start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
+    end_time = time.perf_counter()
     
-    return (end_time - start_time) / n
+    total_time = end_time - start_time
+    return total_time / n
