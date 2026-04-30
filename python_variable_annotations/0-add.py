c01519cc8a7c9a529bd36315f62d149bdfcd86b6
ulvibrahimov <ulvibrahimov@gmail.com>
2026-04-30 14:00:31 +0000

    Task 0: Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides a basic type-annotated function to add two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns the sum.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
