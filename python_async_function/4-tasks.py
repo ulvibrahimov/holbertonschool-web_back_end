219e921adcfae81b2fb1c814f19d75aa99fcfdb1
ulvibrahimov <ulvibrahimov@gmail.com>
2026-04-30 16:42:43 +0000

    Fix Pycodestyle and update Task 4 to match Task 1 logic

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -20,10 +20,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
         List[float]: List of all the delays in ascending order.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = []
-    
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-        
-    return delays
+    return [await task for task in asyncio.as_completed(tasks)]
