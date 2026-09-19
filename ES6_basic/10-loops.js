f2f62be1a5fd8d99e429a114189ba690380cc9bf
ulvibrahimov <ulvibrahimov@github.com>
2026-09-19 15:29:21 +0000

    Complete Task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayCopy = [];
+  for (const value of array) {
+    arrayCopy.push(appendString + value);
+  }
+
+  return arrayCopy;
+}
